558b0405cc378291563bddc6908fd4983697e815
tattoo197@outlook.com <tattoo197@outlook.com>
2021-02-14 21:20:16 +0700

    SectionTitle

--- lib/screens/home/components/body_home.dart
+++ lib/screens/home/components/body_home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travelapp_flutter/constants.dart';
 import 'package:travelapp_flutter/screens/home/components/search_field.dart';
+import 'package:travelapp_flutter/screens/home/components/section_title.dart';
 import 'package:travelapp_flutter/size_config.dart';
 
 import 'home_header.dart';
@@ -13,6 +14,12 @@ class BodyHome extends StatelessWidget {
       child: Column(
         children: [
           HomeHeader(),
+          SizedBox(height: getProportionateScreenWidth(25)),
+          VerticalSpacing(),
+          SectionTitle(
+            title: "Right Now At Spark",
+            press: () {},
+          )
         ],
       ),
     );
